ffcb7709c999f6a56ab72f292a8138626d1bac04
Gohza “Noname” <boykingkao@gmail.com>
2023-11-10 00:29:59 +0700

    no message

--- angular17Test/src/app/app.component.ts
+++ angular17Test/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'angular17Test';
 
 
   data = 5
+  array_Data:number[] = [1,2,3,4,5,6,7]
 }
